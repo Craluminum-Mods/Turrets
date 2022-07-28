3201cd708963f99e4ce78db8108cccb67da52f67
Craluminum2413 <beware.14b@gmail.com>
2022-07-29 00:00:43 +0300

    Add missing and remove unnecessary usings

--- src/Common/Entity/EntityTurret.cs
+++ src/Common/Entity/EntityTurret.cs
@@ -1,12 +1,11 @@
-using System.Collections.Generic;
 using System.Text;
 using Vintagestory.API.Client;
 using Vintagestory.API.Common;
 using Vintagestory.API.Common.Entities;
 using Vintagestory.API.Config;
+using Vintagestory.API.Datastructures;
 using Vintagestory.API.MathTools;
 using Vintagestory.API.Util;
-using Vintagestory.GameContent;
 
 namespace CRTurrets
 {
